cd35c224893107b21d30498b8168c3197adc1ae2
FrancoM195 <francomolina-1996@hotmail.com>
2023-08-10 20:52:22 -0300

    Comprobante inicial sin objeto

--- comprobantepdf/src/app/app.component.ts
+++ comprobantepdf/src/app/app.component.ts
@@ -11,6 +11,9 @@ export class AppComponent implements OnInit {
   src: any;
   documento = new jsPDF({ format: 'a4' });
 
+
+  
+
   Actualizar(){
     const doc = this.documento;
 
@@ -21,7 +24,174 @@ export class AppComponent implements OnInit {
   console.log(pageHeight + ',' + pageWidth);
 
   // // page 1
-  doc.setFont('helvetica', 'bold');
+
+// ENCABEZADO Y MARCO 
+
+
+doc.setFont('helvetica', 'bold');
+doc.setFontSize (6);
+doc.text('Original', 100, 8, { align: 'center' });
+doc.setLineWidth(0.5);
+doc.line (10,5, pageWidth - 10, 5);
+doc.line (10,9, pageWidth - 10, 9);
+doc.line (10,250, pageWidth - 10, 250);
+doc.line (10,5, pageWidth - 200, 250);
+doc.line (pageWidth - 10, 5, pageWidth - 10, 250) ;
+
+doc.line (100, 19, 100, 45);
+doc.rect (90, 9, 20, 10);
+
+
+
+// DATOS VENDEDOR
+
+
+doc.setFont('helvetica', 'bold');
+doc.setFontSize(13);
+doc.text ('Marcelo Nigro', 35, 16);
+doc.text ('Factura', 145, 16);
+doc.setFontSize(14);
+doc.text ('C',  100, 14.5, { align: 'center' });
+doc.setFontSize(6);
+doc.text ('CÓD. 11', 100, 18, { align: 'center' });
+doc.setFontSize(9);
+
+
+doc.text ('Razón Social', 12, 24);
+doc.text ('Domicilio', 12, 29);
+doc.text ('Sit. IVA', 12, 34);
+doc.text ('Teléfono', 12, 39);
+
+doc.text ('Marcelo Nigro', 47, 24);
+doc.text ('Domicilio', 47, 29);
+doc.text ('Responsable / Monotributo', 47, 34);
+doc.text ('353-0000000', 47, 39);
+
+
+
+// PARTE FACTURA
+
+
+doc.text ('Pto.Vta.'     , 132, 24);
+doc.text ('Comp', 162, 24);
+doc.text ('Emisión', 102, 29);
+doc.text ('CUIT', 102, 34)
+doc.text ('II BB', 102, 39);
+doc.text ('Inicio Act.', 102, 44);
+
+doc.text ('0004'     , 150, 24);
+doc.text ('000000003', 175, 24);
+doc.text ('16/02/2023', 132, 29);
+doc.text ('20-20749874-3', 132, 34)
+doc.text ('280203947', 132, 39);
+doc.text ('01/01/2000', 132, 44);
+
+doc.line (10, 45, pageWidth - 10, 45);
+
+
+
+// datos del cliente
+
+doc.text ('Cliente', 12, 50);
+doc.text ('Domicilio', 12, 55);
+doc.text ('Localidad', 12, 60);
+doc.text ('Nro Doc.', 135, 50);
+doc.text ('Sit. IVA',135, 55);
+doc.text ('Cond. Vta.', 135, 60);
+
+doc.text ('0001 - Consumidor Final', 37, 50);
+doc.text ('Domicilio', 37, 55);
+doc.text ('JAMES CRAIK', 37, 60);
+doc.text ('00000000', 155, 50);
+doc.text ('IVA Consumidor Final',155, 55);
+doc.text ('Contado', 155, 60);
+
+doc.line (10, 62.5, pageWidth - 10, 62.5);
+
+
+// ENCABEZADO VENTA
+
+doc.setDrawColor(0);
+doc.setFillColor(155, 155, 155);
+doc.rect(10.3, 63, 189.5, 5, "F");
+
+doc.text ('Código',12, 66.5);
+doc.line (40, 63, 40, 68);
+doc.text ('Artículo', 45, 66.5);
+doc.line (130, 63, 130, 68);
+doc.text ('Cantidad', 136, 66.5);
+doc.line (150, 63, 150, 68);
+doc.text ('Precio', 159.5, 66.5);
+doc.line (170, 63, 170, 68);
+doc.text ('Subtotal', 186, 66.5);
+
+doc.line (10, 68, pageWidth - 10, 68);
+
+
+// tabla de articulos comprados
+
+doc.setFont('helvetica', 'normal');
+doc.text ('555',12, 72);
+doc.text ('Articulo general',45, 72);
+doc.text ('2.00',149, 72, { align: 'right' });
+doc.text ('100.00',169, 72, { align: 'right' });
+doc.text ('200.00',199, 72, { align: 'right' });
+
+doc.text ('556',12, 77);
+doc.text ('Articulo general',45, 77);
+doc.text ('5.00',149, 77, { align: 'right' });
+doc.text ('200.00',169, 77, { align: 'right' });
+doc.text ('1000.00',199, 77, { align: 'right' });
+
+doc.text ('557',12, 82);
+doc.text ('Articulo general',45, 82);
+doc.text ('10.00',149, 82, { align: 'right' });
+doc.text ('2000.00',169, 82, { align: 'right' });
+doc.text ('20000.00',199, 82, { align: 'right' });
+
+
+/* INTENTO DE TABLA
+
+doc.setFillColor(255,255,255);
+doc.setDrawColor(0);
+doc.setLineWidth(0);
+
+var data = [['Nombre', 'Años', 'País'],
+            ['John Doe', 25, 'USA'], 
+            ['Jane Doe', 24, 'Canada']];
+doc.table (10,10, data, 200, 100);
+*/
+
+
+
+// PIE DE PAGINA
+
+
+doc.setFont('helvetica', 'normal');
+doc.setFontSize(8);
+
+doc.text ('CAE', 43, 230);
+doc.text ('73074079431998', 58, 230);
+doc.text ('Vto. CAE', 43, 235);
+doc.text ('26/02/2023', 58, 235);
+
+doc.setFont('helvetica', 'bold');
+doc.text ('Comprobante autorizado', 61, 240);
+
+doc.setFontSize(9);
+doc.text ('Subtotal',120,228);
+doc.text ('21200.00',195,228, { align: 'right' });
+doc.text ('Total',120,245);
+doc.text ('21200.00',195,245, { align: 'right' });
+
+doc.line (10, 215, 200, 215 )
+doc.line (100, 250, 100, 215)
+
+doc.addImage("https://previews.123rf.com/images/vinntom/vinntom1202/vinntom120200028/12430323-c%C3%B3digo-qr.jpg", "jpg", 12,218,28,28);
+
+
+
+/*   doc.setFont('helvetica', 'bold');
   doc.setFontSize(14);
   doc.text('Prueba DAERAH AIR MINUM', pageWidth / 2, 16, {
     align: 'center',
@@ -29,7 +199,6 @@ export class AppComponent implements OnInit {
   doc.setFontSize(16);
   doc.text('TIRTA MAHAKAM', pageWidth / 2, 22, { align: 'center' });
   doc.setFontSize(14);
-  doc.text('CABANG TENGGARONG', pageWidth / 2, 28, { align: 'center' });
   doc.setLineWidth(0.5);
   doc.line(10, 5, pageWidth - 10, 5);
   doc.line(10, 35, pageWidth - 10, 35);
@@ -89,7 +258,7 @@ export class AppComponent implements OnInit {
 
   doc.setFont('helvetica', 'bold');
   doc.text('PELANGGAN', pageWidth / 2 + 50, 185, { align: 'center' });
-  doc.text('PURWANTO', pageWidth / 2, 230, { align: 'center' });
+  doc.text('PURWANTO', pageWidth / 2, 230, { align: 'center' }); */
 
 
   // generate
